59a66c1fdb3435c867652f983756c23fec71fc8c
Jegajeeth <jegajeeth.manikandan@increscotech.com>
2022-11-02 10:50:00 +0530

    feat(login): keyboard popup

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {KeyboardAvoidingView, Platform} from 'react-native';
 import Header from '../components/common/Header';
 import ScreenContainer from '../components/common/ScreenContainer';
 import LoginBox from '../components/loginLogout/LoginBox';
@@ -8,7 +9,10 @@ const Login = () => {
     <>
       <Header />
       <ScreenContainer>
-        <LoginBox />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'android' ? 'height' : 'padding'}>
+          <LoginBox />
+        </KeyboardAvoidingView>
       </ScreenContainer>
     </>
   );
